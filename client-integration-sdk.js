84ff16bf5b4963e6e1763e7482527a8214591597
Abhishek Jha <abhi.jha.cs@gmail.com>
2024-02-11 21:51:21 +0530

    Added code for client integration SDK

---
+++ client-integration-sdk.js
@@ -0,0 +1,76 @@
+/*
+This file contains code for client side integration SDK
+User needs to load this JS file in their project (Will serve it via some CDN)
+and when this SDK is loaded, they can use the below syntax to capture event
+
+const response = await window.sankhyaSDKv1.captureUserEvent("api-key-here");
+
+This is all that customer has to do after setting up their project
+*/
+
+function captureSankhyaEvent(apiKey) {
+	return new Promise(async (resolve, reject) => {
+		try {
+			if (typeof window === "undefined") {
+				return reject("You can use Sankhya only on client side.");
+			}
+
+			if (checkIfEventAlreadyCaptured()) {
+				return reject("You have already captured an event.");
+			}
+
+			const url = "http://localhost:3000/api/v1/event";
+			const options = {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					"x-api-key": apiKey,
+				},
+				body: JSON.stringify({
+					screenResolution: `${screen.width}x${screen.height}`,
+					operatingSystem: getClientOperatingSystem(),
+				}),
+			};
+			const response = await fetch(url, options);
+			const data = await response.json();
+			updateEventCaptureStatus();
+			resolve(data);
+		} catch (error) {
+			reject(
+				error.message ??
+					"Failed to capture event. Please try again later."
+			);
+		}
+	});
+}
+
+function getClientOperatingSystem() {
+	const userAgent = navigator.userAgent;
+	const popularOSList = [
+		{ name: "Android", value: "Android" },
+		{ name: "iOS", value: "iPhone" },
+		{ name: "MacOS", value: "Mac" },
+		{ name: "Linux", value: "Linux" },
+		{ name: "Windows", value: "Win" },
+	];
+
+	for (const os of popularOSList) {
+		if (userAgent.includes(os.value)) {
+			return os.name;
+		}
+	}
+	return "Unknown";
+}
+
+function checkIfEventAlreadyCaptured() {
+	const captureStatus = sessionStorage.getItem("sankhya-event-status");
+	return captureStatus && captureStatus === "captured";
+}
+
+function updateEventCaptureStatus() {
+	sessionStorage.setItem("sankhya-event-status", "captured");
+}
+
+window.sankhyaSDKv1 = {
+	captureUserEvent: captureSankhyaEvent,
+};
