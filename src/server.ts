67073dac0a745ef0787e34c550d42bb286cc6d75
Abhishek Jha <abhi.jha.cs@gmail.com>
2024-03-21 21:43:25 +0530

    fix corsOptions syntax

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors";
+import cors, { CorsOptions } from "cors";
 
 import authRoute from "./routes/auth";
 import customerRoute from "./routes/customer";
@@ -14,12 +14,11 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-const corsOptions = {
+const corsOptions: CorsOptions = {
 	origin: process.env.SANKHYA_FRONTEND_URL,
 	credentials: true,
-	"Access-Control-Allow-Headers": "X-PINGOTHER, Content-Type",
-	"Access-Control-Allow-Methods":
-		"GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS",
+	methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
+	allowedHeaders: ["Content-Type", "Authorization", "X-PINGOTHER"],
 };
 
 app.use("/api/v1/auth", cors(corsOptions), authRoute);
